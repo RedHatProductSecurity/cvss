d1116243f1e153e3e2c821b89aefe022840b2efd
Viktória Tóthová <vtothova@redhat.com>
2018-07-20 16:36:04 +0200

    Add support for parsing CVSS2 from text

--- cvss/parser.py
+++ cvss/parser.py
@@ -1,18 +1,23 @@
 import re
 
-from cvss import CVSS3
-from cvss.exceptions import CVSS3MalformedError
+from cvss import CVSS3, CVSS2
+from cvss.exceptions import CVSSError
 
 
 def parse_cvss_from_text(text):
-    matches = re.compile(r'(CVSS:3.0/[A-Z:/]+)').findall(text)
+    # :? at the beginning of paren () -> grouping is not extracted
+    matches = re.compile(r'((?:CVSS:3\.0)?[A-Za-z:/]+)').findall(text)
 
-    cvss3s = []
+    cvsss = []
     for match in matches:
         try:
-            cvss3 = CVSS3(match)
-            cvss3s.append(cvss3)
-        except (CVSS3MalformedError, KeyError):
+            if match.startswith('CVSS:3.0'):
+                cvss = CVSS3(match)
+            else:
+                cvss = CVSS2(match)
+
+            cvsss.append(cvss)
+        except (CVSSError, KeyError):
             pass
 
-    return cvss3s
+    return cvsss
