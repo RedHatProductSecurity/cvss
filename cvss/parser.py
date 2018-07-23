5a611eab23482c7d6d6ae35a9632c27400cc54c2
Viktória Tóthová <vtothova@redhat.com>
2018-07-23 10:28:04 +0200

    Add check for min vector length for efficiency

--- cvss/parser.py
+++ cvss/parser.py
@@ -6,18 +6,22 @@ from cvss.exceptions import CVSSError
 
 def parse_cvss_from_text(text):
     # :? at the beginning of paren () -> grouping is not extracted
-    matches = re.compile(r'((?:CVSS:3\.0)?[A-Za-z:/]+)').findall(text)
+    # Match may start with 'CVSS:3.0/'
+    # Followed by CV
+    matches = re.compile(r'((?:CVSS:3\.0/)?[A-Z][A-Za-z:/]+[A-Z])').findall(text)
 
+    min_vector_length = 26
     cvsss = []
     for match in matches:
-        try:
-            if match.startswith('CVSS:3.0'):
-                cvss = CVSS3(match)
-            else:
-                cvss = CVSS2(match)
+        if len(match) >= min_vector_length:
+            try:
+                if match.startswith('CVSS:3.0'):
+                    cvss = CVSS3(match)
+                else:
+                    cvss = CVSS2(match)
 
-            cvsss.append(cvss)
-        except (CVSSError, KeyError):
-            pass
+                cvsss.append(cvss)
+            except (CVSSError, KeyError):
+                pass
 
     return cvsss
